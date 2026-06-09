562f92024c324944b47e4067b83590726b0f64cf
Swati2706-kumawat <swatikumawat748@gmail.com>
2026-06-09 13:35:49 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,8 @@
+class shivani{
+    public static void main(String args[]){
+       System.out.println("Hello Everyone !!");
+       int a=10,b=20;
+       int c=a+b;
+       System.out.println("Sum is "+c);
+    }
+}
